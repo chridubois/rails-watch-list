7658b25d0de76b4cdcf01c2eea5a8a41154b9b0d
Christophe Dubois <idategto11@gmail.com>
2023-02-24 11:36:42 +0100

    fixes

--- app/javascript/controllers/typed_js_controller.js
+++ app/javascript/controllers/typed_js_controller.js
@@ -1,14 +1,14 @@
-import { Controller } from "@hotwired/stimulus"
-import Typed from "typed.js"
+// import { Controller } from "@hotwired/stimulus"
+// import Typed from "typed.js"
 
-export default class extends Controller {
-  connect() {
-    new Typed(this.element, {
-      strings: ['Drama', 'Comic', 'Fantasy', 'Horror'],
-    typeSpeed: 150,
-      backSpeed: 0,
-      smartBackspace: true, // this is a default
-      loop: true
-    })
-  }
-}
+// export default class extends Controller {
+//   connect() {
+//     new Typed(this.element, {
+//       strings: ['Drama', 'Comic', 'Fantasy', 'Horror'],
+//       typeSpeed: 150,
+//       backSpeed: 0,
+//       smartBackspace: true, // this is a default
+//       loop: true
+//     })
+//   }
+// }
