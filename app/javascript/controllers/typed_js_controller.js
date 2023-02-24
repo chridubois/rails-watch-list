74232df08340fa277e0be7d40574f270e978b037
Christophe Dubois <idategto11@gmail.com>
2023-02-24 11:24:58 +0100

    trying fixes for heroku

--- app/javascript/controllers/typed_js_controller.js
+++ app/javascript/controllers/typed_js_controller.js
@@ -4,8 +4,10 @@ import Typed from "typed.js"
 export default class extends Controller {
   connect() {
     new Typed(this.element, {
-      strings: ["Save any kind of movies", "Enjoy !!!!!!!!!!!"],
-      typeSpeed: 10,
+      strings: ['Drama', 'Comic', 'Fantasy', 'Horror'],
+    typeSpeed: 150,
+      backSpeed: 0,
+      smartBackspace: true, // this is a default
       loop: true
     })
   }
